9d1f277e37d9a20404e26cf0127da5fa521cd297
kjurdzinski <k.jurdzinski@gmail.com>
2023-05-21 21:54:35 +0200

    Remove dbOTU from environment, add 18S step to workflow

--- SLURM_chimera_dbOTU.sh
+++ SLURM_chimera_dbOTU.sh
@@ -5,9 +5,10 @@
 #SBATCH -n 20
 #SBATCH --mail-type=END
 #SBATCH --mail-user=krzysztof.jurdzins@scilifelab.se
-#SBATCH -t 06:00:00
+#SBATCH -t 12:00:00
 #SBATCH -J chimera_dbOTU_clustering
 
 cd /crex/proj/snic2020-6-126/projects/plankton_monitoring/P20310/ASV_reannotation/chimera_dbOTU_pipeline
 conda activate chimera-dbOTU-pipeline
 bash workflow_all.sh 16S
+bash workflow_all.sh 18S
