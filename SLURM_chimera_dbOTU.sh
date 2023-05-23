5f1f8f8fb8703b3b67c70a16cc9eadbe67a4c9f0
kjurdzinski <k.jurdzinski@gmail.com>
2023-05-23 10:50:32 +0200

    Correct SLURM code to be compatible with previous changes, change SLuRM runtime

--- SLURM_chimera_dbOTU.sh
+++ SLURM_chimera_dbOTU.sh
@@ -5,10 +5,10 @@
 #SBATCH -n 20
 #SBATCH --mail-type=END
 #SBATCH --mail-user=krzysztof.jurdzins@scilifelab.se
-#SBATCH -t 12:00:00
+#SBATCH -t 06:00:00
 #SBATCH -J chimera_dbOTU_clustering
 
 cd /crex/proj/snic2020-6-126/projects/plankton_monitoring/P20310/ASV_reannotation/chimera_dbOTU_pipeline
 conda activate chimera-dbOTU-pipeline
 bash workflow_all.sh 16S -d 0.03 -p 0.001
-bash workflow_all.sh 18S -d 0.1
+bash workflow_all.sh 18S -d 0.1 -p 0.0005
