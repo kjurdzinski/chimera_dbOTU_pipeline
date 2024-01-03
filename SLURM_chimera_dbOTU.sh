5396a235dc064fd26d9fac498bac2b1a27c91820
kjurdzinski <k.jurdzinski@gmail.com>
2024-01-03 23:58:51 +0100

    Change SLURM email mode to ALL§

--- SLURM_chimera_dbOTU.sh
+++ SLURM_chimera_dbOTU.sh
@@ -3,12 +3,12 @@
 #SBATCH -A naiss2023-5-110
 #SBATCH -p core
 #SBATCH -n 20
-#SBATCH --mail-type=END
+#SBATCH --mail-type=ALL
 #SBATCH --mail-user=krzysztof.jurdzins@scilifelab.se
 #SBATCH -t 06:00:00
 #SBATCH -J chimera_dbOTU_clustering
 
-cd /crex/proj/snic2020-6-126/projects/plankton_monitoring/P20310/ASV_reannotation/chimera_dbOTU_pipeline
+cd /crex/proj/snic2020-6-126/projects/plankton_monitoring/seq_2015_2017_analysis/chimera_dbOTU_pipeline/
 conda activate chimera-dbOTU-pipeline
 bash workflow_all.sh 16S -d 0.03 -p 0.0005
 bash workflow_all.sh 18S -d 0.1 -p 0.0005
