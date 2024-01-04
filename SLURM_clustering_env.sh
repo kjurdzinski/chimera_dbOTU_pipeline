e4ff5f496af6aab794be9afca0b993f6392b3448
kjurdzinski <k.jurdzinski@gmail.com>
2024-01-04 15:00:03 +0100

    Uncomment SLURM_clustering_env.sh

--- SLURM_clustering_env.sh
+++ SLURM_clustering_env.sh
@@ -3,13 +3,13 @@
 #SBATCH -A naiss2023-5-110
 #SBATCH -p core
 #SBATCH -n 20
-#SBATCH --mail-type=END
+#SBATCH --mail-type=ALL
 #SBATCH --mail-user=krzysztof.jurdzins@scilifelab.se
 #SBATCH -t 00:40:00
 #SBATCH -J creating_ASV_clustering_env
 
 cd /crex/proj/snic2020-6-126/projects/plankton_monitoring/P20310/ASV_reannotation/chimera_dbOTU_pipeline
 conda env create -f environment.yml
-# conda activate chimera-dbOTU-pipeline
-# conda install -c cduvallet dbotu
+conda activate chimera-dbOTU-pipeline
+conda install -c cduvallet dbotu
 
