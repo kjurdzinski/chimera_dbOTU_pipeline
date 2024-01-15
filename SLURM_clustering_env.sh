3be044987e57ade75d7396b913a5a3285b9fa55c
kjurdzinski <k.jurdzinski@gmail.com>
2024-01-15 19:43:07 +0100

    Correct workflow file

--- SLURM_clustering_env.sh
+++ SLURM_clustering_env.sh
@@ -8,7 +8,7 @@
 #SBATCH -t 00:40:00
 #SBATCH -J creating_ASV_clustering_env
 
-cd /crex/proj/snic2020-6-126/projects/plankton_monitoring/P20310/ASV_reannotation/chimera_dbOTU_pipeline
+cd /crex/proj/snic2020-6-126/projects/plankton_monitoring/chimera_dbOTU_pipeline/
 conda env create -f environment.yml
 conda activate chimera-dbOTU-pipeline
 conda install -c cduvallet dbotu
--- workflow_all.sh
+++ workflow_all.sh
@@ -1,34 +1,47 @@
-if [ ! -d "chimeras_$1" ] 
+while getopts t:d:p: flag
+do
+    case "${flag}" in
+        t) datatype=${OPTARG};;
+        d) genetic=${OPTARG};;
+        p) distribution=${OPTARG};;
+    esac
+done
+
+echo $datatype
+echo $genetic
+echo $distribution
+
+if [ ! -d "chimeras_$datatype" ] 
 then
-    mkdir chimeras_$1
+    mkdir chimeras_$datatype
 fi
-if [ ! -d "temporary_$1" ] 
+if [ ! -d "temporary_$datatype" ] 
 then
-    mkdir temporary_$1
+    mkdir temporary_$datatype
 fi
-if [ ! -d "logs_$1" ] 
+if [ ! -d "logs_$datatype" ] 
 then
-    mkdir logs_$1
+    mkdir logs_$datatype
 fi
-python3 scripts/adjust_count_table_format.py -c data_$1/asv_counts.tsv
-python3 scripts/sort_ASVs_by_abundance.py data_$1/asv_seqs.fasta \
-     -c data_$1/asv_counts.tsv -o temporary_$1/asv_seqs_sorted.fasta
+python3 scripts/adjust_count_table_format.py -c data_$datatype/asv_counts.tsv
+python3 scripts/sort_ASVs_by_abundance.py data_$datatype/asv_seqs.fasta \
+     -c data_$datatype/asv_counts.tsv -o temporary_$datatype/asv_seqs_sorted.fasta
 vsearch --threads 6 --dn 1.4 --mindiffs 3 \
                 --mindiv 0.8 --minh 0.28 --abskew 2.0 \
-                --chimeras chimeras_$1/chimeras.fasta --borderline chimeras_$1/borderline.fasta \
-                --nonchimeras chimeras_$1/nonchimeras.fasta --uchimealns chimeras_$1/uchimealns.out \
-                --uchimeout chimeras_$1/uchimeout.txt --uchimealns chimeras_$1/uchimealns.txt \
-                --uchime_denovo temporary_$1/asv_seqs_sorted.fasta  \
-                >logs_$1/log_uchime.txt 2>&1
-if [ ! -d "OTUs_$1" ] 
+                --chimeras chimeras_$datatype/chimeras.fasta --borderline chimeras_$datatype/borderline.fasta \
+                --nonchimeras chimeras_$datatype/nonchimeras.fasta --uchimealns chimeras_$datatype/uchimealns.out \
+                --uchimeout chimeras_$datatype/uchimeout.txt --uchimealns chimeras_$datatype/uchimealns.txt \
+                --uchime_denovo temporary_$datatype/asv_seqs_sorted.fasta  \
+                >logs_$datatype/log_uchime.txt 2>&1
+if [ ! -d "OTUs_$datatype" ] 
 then
-    mkdir OTUs_$1
+    mkdir OTUs_$datatype
 fi
 # echo "\"\"\t" | cat asv_counts.txt
-python3 scripts/remove_size_from_chimera_fasta.py chimeras_$1/nonchimeras.fasta \
-    -o chimeras_$1/nonchimeras_clean.fasta
-python3 scripts/adjust_ASV_counts_for_dbOTU.py data_$1/asv_counts.tsv \
-     -f chimeras_$1/nonchimeras_clean.fasta -o temporary_$1/asv_counts_dbOTU.tsv
-python3 scripts/dbotu3.py temporary_$1/asv_counts_dbOTU.tsv chimeras_$1/nonchimeras_clean.fasta \
-    -o OTUs_$1/OTUs.txt --membership OTUs_$1/OTUs_membership.txt --log logs_$1/OTU_log.txt \
-    -d $d -a 0 -p $p
+python3 scripts/remove_size_from_chimera_fasta.py chimeras_$datatype/nonchimeras.fasta \
+    -o chimeras_$datatype/nonchimeras_clean.fasta
+python3 scripts/adjust_ASV_counts_for_dbOTU.py data_$datatype/asv_counts.tsv \
+     -f chimeras_$datatype/nonchimeras_clean.fasta -o temporary_$datatype/asv_counts_dbOTU.tsv
+python3 scripts/dbotu3.py temporary_$datatype/asv_counts_dbOTU.tsv chimeras_$datatype/nonchimeras_clean.fasta \
+    -o OTUs_$datatype/OTUs.txt --membership OTUs_$datatype/OTUs_membership.txt --log logs_$datatype/OTU_log.txt \
+    -d $genetic -a 0 -p $distribution
